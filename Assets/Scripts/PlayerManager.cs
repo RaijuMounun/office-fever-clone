f246d21020dd2038ba011ab804c5081c7c413646
Eren Keskinoğlu <erenkeskinoglu@outlook.com>
2022-10-26 09:28:06 +0300

    Made Printer

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 direction;
+    private Camera cam;
+    private Animator playerAnimator;
+    [FoldoutGroup("Player"), SerializeField] private float playerSpeed = 20f;
+
+    private void Awake()
     {
-        
+        cam = Camera.main;
+        playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, transform.position);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (plane.Raycast(ray, out var distance))
+                direction = ray.GetPoint(distance);
+
+
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(direction.x, 0f, direction.z), playerSpeed * Time.deltaTime);
+
+            #region Fix
+            //If the mouse is over the character, the character lies on the ground.
+            var offset = direction - transform.position;
+
+            if (offset.magnitude > .1f)
+                transform.LookAt(direction);
+            #endregion
+            
+        }
+
+        #region Running Anim
+        if (Input.GetMouseButton(0))
+        {
+            playerAnimator.SetBool("isRunning", true);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            playerAnimator.SetBool("isRunning", false);
+        }
+        #endregion
     }
 }
---
+++ Assets/Scripts/Printer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Printer : MonoBehaviour
+{
+    [SerializeField] private Transform[] papersPlace = new Transform[10];
+    [SerializeField] private GameObject paper;
+    [SerializeField] private float paperDeliveryTime, YAxis;
+
+    private void Awake()
+    {
+        for (int i = 0; i < papersPlace.Length; i++)
+        {
+            papersPlace[i] = transform.GetChild(0).GetChild(i);
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PrintPaper(paperDeliveryTime));
+    }
+
+
+    public IEnumerator PrintPaper(float Time)
+    {
+        var countPapers = 0;
+        var PP_index = 0;
+
+        while (countPapers < 100)
+        {
+            GameObject NewPaper = Instantiate(paper, new Vector3(transform.position.x, 0, transform.position.z), Quaternion.identity, transform.GetChild(1));
+
+            NewPaper.transform.DOJump(new Vector3(papersPlace[PP_index].position.x, papersPlace[PP_index].position.y + YAxis, papersPlace[PP_index].position.z), 2f, 1, .5f).SetEase(Ease.OutQuad);
+
+            if (PP_index < 9)
+                PP_index++;
+            else
+            {
+                PP_index = 0;
+                YAxis += .026f;
+            }
+
+            yield return new WaitForSecondsRealtime(Time);
+        }
+    }
+}
